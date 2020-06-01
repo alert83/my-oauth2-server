116de16969d89f193b9bc4889c0b72b67f1aec0f
Yevgeniy Li <alert83@gmail.com>
2020-06-02 00:34:35 +0600

    up

--- index.ts
+++ index.ts
@@ -26,67 +26,60 @@ config();
 const PORT = process.env.PORT || 5000
 const app = express();
 
-if (isProd()) {
-    Sentry.init({dsn: process.env.SENTRY_DSN});
-
-    // Sentry request handler must be the first middleware on the app
-    app.use(Sentry.Handlers.requestHandler());
-}
+Sentry.init({dsn: process.env.SENTRY_DSN});
 
 const container = new Container({defaultScope: "Singleton"});
 container.load(buildProviderModule());
 container.bind(TYPE.Application).toConstantValue(app);
 
 const server = new InversifyExpressServer(container, null, null, app);
-server.setConfig((_app) => {
-    _app
-        .use(session({
-            secret: "38240a30-5ed7-41f2-981c-4a9603f332f2",
-            resave: false,
-            saveUninitialized: true,
-            cookie: {secure: false}
-        }))
-        .use(json())
-        .use(urlencoded({extended: false}))
-        .use(express.static(join(__dirname, 'public')))
+server
+    .setConfig((_app) => {
+        _app
+            // Sentry request handler must be the first middleware on the app
+            .use(Sentry.Handlers.requestHandler())
+            .use(session({
+                secret: "38240a30-5ed7-41f2-981c-4a9603f332f2",
+                resave: false,
+                saveUninitialized: true,
+                cookie: {secure: false}
+            }))
+            .use(json())
+            .use(urlencoded({extended: false}))
+            .use(express.static(join(__dirname, 'public')))
+        //
+        _app
+            .set('ioc container', container)
+            .set('oauth2', new OAuth2Server({
+                model: container.get<OAuth2Model>(TYPE.OAuth2Model),
+                authorizationCodeLifetime: isProd() ? Number(process.env.CODE_LIFETIME ?? 5 * 60) : 5,
+                accessTokenLifetime: isProd() ? Number(process.env.ACCESS_TOKEN_LIFETIME ?? 2 * 60 * 60) : 5,
+                refreshTokenLifetime: isProd() ? Number(process.env.REFRESH_TOKEN_LIFETIME ?? 14 * 24 * 60 * 60) : 5,
+            }))
+            .set('views', join(__dirname, 'views'))
+            .set('view engine', 'ejs')
+    })
+    .setErrorConfig((_app) => {
+        _app
+            // Sentry error handler must be before any other error middleware and after all controllers
+            .use(Sentry.Handlers.errorHandler({
+                shouldHandleError(error) {
+                    // Capture all 404 and 500 errors
+                    return Number(error.status) >= 400;
+                }
+            }))
+            .use(errorHandler({
+                debug: isDev(app),
+                log: true,
+            }))
+    })
+    .build()
+    .listen(PORT, () => {
+        console.log(`Listening on ${PORT}`);
+        console.log(`env:`, process.env.NODE_ENV);
 
-    //
-
-    if (isProd()) {
-        // Sentry error handler must be before any other error middleware and after all controllers
-        app.use(Sentry.Handlers.errorHandler({
-            shouldHandleError(error) {
-                // Capture all 404 and 500 errors
-                return Number(error.status) >= 400;
-            }
-        }))
-    }
-
-    app.use(errorHandler({
-        debug: isDev(app),
-        log: true,
-    }))
-
-    //
-    _app
-        .set('ioc container', container)
-        .set('oauth2', new OAuth2Server({
-            model: container.get<OAuth2Model>(TYPE.OAuth2Model),
-            authorizationCodeLifetime: isProd() ? Number(process.env.CODE_LIFETIME ?? 5 * 60) : 5,
-            accessTokenLifetime: isProd() ? Number(process.env.ACCESS_TOKEN_LIFETIME ?? 2 * 60 * 60) : 5,
-            refreshTokenLifetime: isProd() ? Number(process.env.REFRESH_TOKEN_LIFETIME ?? 14 * 24 * 60 * 60) : 5,
-        }))
-        .set('views', join(__dirname, 'views'))
-        .set('view engine', 'ejs')
-    ;
-});
-server.build();
-app.listen(PORT, () => {
-    console.log(`Listening on ${PORT}`);
-    console.log(`env:`, process.env.NODE_ENV);
-
-    if (isProd()) {
-        const wd = container.get<WatchDogService>(TYPE.WatchDogService);
-        wd.process();
-    }
-});
+        if (isProd()) {
+            const wd = container.get<WatchDogService>(TYPE.WatchDogService);
+            wd.process();
+        }
+    });
