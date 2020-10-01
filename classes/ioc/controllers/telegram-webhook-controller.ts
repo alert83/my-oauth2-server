37a4a15a9393c5c4156591e1c0904fb2c99b1168
Yevgeniy Li <alert83@gmail.com>
2020-10-01 15:47:50 +0600

    up

--- classes/ioc/controllers/telegram-webhook-controller.ts
+++ classes/ioc/controllers/telegram-webhook-controller.ts
@@ -1,15 +1,18 @@
 import {BaseHttpController, controller, httpGet, httpPost, request, response} from 'inversify-express-utils';
-import {Request, Response} from "express";
+import {Express, Request, Response} from "express";
 import {inject} from "inversify";
 
 import {TYPE} from "../const";
 import {xAuthIsValid} from "../middlewares";
 import {WatchDogService} from "../../watchDogService";
+import got from "got";
+import * as Sentry from "@sentry/node";
 
 @controller('/twh', xAuthIsValid())
 class TelegramWebhookController extends BaseHttpController {
 
     constructor(
+        @inject(TYPE.Application) private readonly app: Express,
     ) {
         super();
     }
@@ -20,6 +23,20 @@ class TelegramWebhookController extends BaseHttpController {
         @response() res: Response,
     ) {
         console.log(req.body);
+
+        const redis = this.app.get('redis');
+        await got.post(await redis.get("ngrok") ?? '', {
+            username: process.env.NGROK_USER,
+            password: process.env.NGROK_PASS,
+            json: req.body,
+        }).catch((err) => {
+            try {
+                Sentry.captureException(err);
+            } catch (e) {
+                console.error(e);
+            }
+        });
+
         return res.status(200).send(true).end();
     }
 
